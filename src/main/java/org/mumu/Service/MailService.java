d5f94d6089e179fc33b4987d173dca09a563045a
muhaiminmuhyi <muhaiminmuhyi@gmail.com>
2023-03-14 16:58:51 +0700

    added scheduler and configuration scheduler

--- src/main/java/org/mumu/Service/MailService.java
+++ src/main/java/org/mumu/Service/MailService.java
@@ -4,10 +4,13 @@ import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.MailTemplate;
 import io.quarkus.mailer.reactive.ReactiveMailer;
 import io.quarkus.qute.Location;
+import io.quarkus.scheduler.Scheduled;
 import io.smallrye.mutiny.Uni;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
+import java.io.File;
+import java.time.LocalDate;
 
 @ApplicationScoped
 public class MailService {
@@ -19,10 +22,13 @@ public class MailService {
     @Location("emailFarm")
     MailTemplate emailFarm;
 
+    @Scheduled(cron="00 07 31 * *")
     public Uni<Void> sendMailFarm(){
+        LocalDate pdfDate = LocalDate.now();
         return emailFarm.to("muhaiminmuhyi@gmail.com")
                 .subject("Report Monthly Farmer")
                 .data("name", "Dengklek")
+                .addAttachment("ReportFarmMonthly.pdf", new File("external_resources/generatedReport/reportOfFarm_"+pdfDate+".pdf"),"application/pdf")
                 .send();
     }
 }
